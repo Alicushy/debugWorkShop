fde76f74cd9d563bc67ea84eeca544ff9c3acccb
unknown <mahshevik@gmail.com>
2025-12-03 19:48:44 +0200

    feat : working loop

---
+++ loop.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+
+int main()
+{
+	unsigned int size = 0;
+
+	int t1 = 0, t2 = 1;
+
+	std::cout << "what is the size of the series? ";
+	std::cin >> size;
+
+	while (size > 0)//the problem was that the while loop condition was incorrect becaused it was "size >= 0" and size is unsigned int so it would never be negative causing an infinite loop
+	{
+		std::cout << t1 << ", ";
+
+		int nextTerm = t1 + t2;
+		t1 = t2;
+		t2 = nextTerm;
+
+		size--;
+	}
+
+	return 0;
+}
